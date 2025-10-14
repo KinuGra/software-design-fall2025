e6e422753051f2f85cfdf978d0b8e912a28664c5
air_kinu <techgit1024@gmail.com>
2025-10-14 16:56:35 +0900

    update: L4 練習問題2

--- L4/practice2.py
+++ L4/practice2.py
@@ -1,8 +1,6 @@
-n = int(input())
-
-
 def FizzBuzz(n):
-    for i in range(1, n):
+    num = 0
+    for i in range(1, n + 1):
         if i % 15 == 0:
             print("FizzBuzz")
         elif i % 5 == 0:
@@ -11,6 +9,12 @@ def FizzBuzz(n):
             print("Fizz")
         else:
             print(i)
+        num = i
+    return num
 
 
-FizzBuzz(n)
+while True:
+    n = int(input())
+    num = FizzBuzz(n)
+    if num % 15 == 0:
+        break
