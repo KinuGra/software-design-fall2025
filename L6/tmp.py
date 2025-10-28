495e01dc823d4e4fb45898da2a9853c8a45e4a09
air_kinu <techgit1024@gmail.com>
2025-10-28 10:07:47 +0900

    add: L6 tmp.pyでdict, setなどを書いてみた

---
+++ L6/tmp.py
@@ -0,0 +1,13 @@
+# リスト
+a = [1, "a"]
+for i in a:
+    print(i, end=" ")
+print()
+
+# 連想配列
+d = {"name": "John", "age": 25}
+print(d.keys(), d.values())
+
+# 集合
+s = {1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 1, 1, 1, 1, 8}
+print(s)
