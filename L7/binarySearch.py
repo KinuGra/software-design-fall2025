55d3327ca15b8421f50f49db76de4ee02d399a28
air_kinu <techgit1024@gmail.com>
2025-11-04 10:23:03 +0900

    fix: 二分探索のバグを修正

--- L7/binarySearch.py
+++ L7/binarySearch.py
@@ -8,19 +8,19 @@ count = 0
 target = int(input("Enter the number to search for. >> "))
 result = None
 
-while left < right:
+while left <= right:
+    count += 1
     print(f"count: {count}, left: {left}, right: {right}, mid: {mid}")
 
     if target == arr[mid]:
         result = mid
         break
 
-    if target > mid:
+    if target > arr[mid]:
         left = mid + 1
     else:
         right = mid - 1
     mid = (left + right) // 2
-    count += 1
 
 if result is None:
     print("Not Found")
