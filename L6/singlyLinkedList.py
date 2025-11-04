06935f879bbde3df524432ec8278a23b4ba305d3
air_kinu <techgit1024@gmail.com>
2025-11-04 09:05:21 +0900

    add: 単方向リストの処理時間計測処理を追加

--- L6/singlyLinkedList.py
+++ L6/singlyLinkedList.py
@@ -1,3 +1,6 @@
+import time
+
+
 class NodeNull:
     pass
 
@@ -19,13 +22,19 @@ class Node:
         current_node.next = Node(value)
 
 
+start = time.perf_counter()
+
 node = Node(1)
-node.add(2)
-node.add(3)
-node.add(4)
+for i in range(2, 10000 + 1):
+    node.add(i)
+
+end = time.perf_counter()
+
 current_node = node
 print("[", end="")
 while current_node.next != NULL:
     print(current_node.value, ", ", end="")
     current_node = current_node.next
 print(f"{current_node.value}]")
+
+print(f"処理時間：{end - start:.6f} 秒")
